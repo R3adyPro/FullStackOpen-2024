aa2f7c0ea4440d7f3c40cb951c4d734a34dbaf6b
R3adyPro <tatu.miinalainen@gmail.com>
2024-09-18 18:56:19 +0300

    2.1-2.5

--- Part1/kurssitiedot/src/App.jsx
+++ Part1/kurssitiedot/src/App.jsx
@@ -13,14 +13,11 @@ const Part = (props) => {
 };
 
 const Content = ({ parts }) => {
-  console.log(parts.name);
   return (
     <div>
-      <ul>
-        {parts.map((part) => (
-          <Part key={part.id} exercises={part.exercises} name={part.name} />
-        ))}
-      </ul>
+      {parts.map((part) => (
+        <Part key={part.id} exercises={part.exercises} name={part.name} />
+      ))}
     </div>
   );
 };
@@ -34,64 +31,26 @@ const Total = ({ parts }) => {
 const Course = (props) => {
   return (
     <div>
-      <Header course={props.courses.name} />
+      <Header course={props.course.name} />
       <Content parts={props.course.parts} />
       <Total parts={props.course.parts} />
     </div>
   );
 };
 
-const App = () => {
-  const courses = [
-    {
-      name: "Half Stack application development",
-      id: 1,
-      parts: [
-        {
-          name: "Fundamentals of React",
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: "Using props to pass data",
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: "State of a component",
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: "Redux",
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      name: "Node.js",
-      id: 2,
-      parts: [
-        {
-          name: "Routing",
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: "Middlewares",
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ];
-
+const CourseLines = (props) => {
   return (
     <div>
-      <Course course={courses} />
+      {props.courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
     </div>
   );
+};
+
+const App = (props) => {
+  const { courses } = props
+  return <CourseLines courses={courses} />;
 };
 
 export default App;
